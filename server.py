f69b2ecd9f6607e80fac404cb086ad43cab5fafd
Bence <arvai.bence1@gmail.com>
2018-11-19 14:39:24 +0100

    Route to question list page

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+import data_manager
+import connection
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return redirect('/list')
+
+
+@app.route('/list')
+def route_list():
+    questions = connection.import_database("quesion")
+    return render_template('list.html', questions=questions)
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
