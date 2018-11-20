011ab049e6386720c12ebc9165e1a2b3d1d20f28
Katalin Csortos <katalin.csortos@gmail.com>
2018-11-20 13:23:26 +0100

    fix: question header

--- connection.py
+++ connection.py
@@ -5,9 +5,8 @@ import uuid
 import time
 
 QUESTION_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
-QUESTION_HEADERS = ['id', 'submission_time',
-                    'view_number', 'vote_number', 'title',
-                    'question_id', 'message', 'image']
+QUESTION_HEADERS = ['id', 'submission_time', 'view_number',
+                    'vote_number', 'title', 'message', 'image']
 ANSWER_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/answer.csv'
 ANSWER_HEADERS = ['id', 'submission_time', 'view_number', 'vote_number', 'question_id', 'message', 'image']
 
