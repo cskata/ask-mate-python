dc93d4afc0f66bb6b1ecd73af51e2627dd921aae
Bence <arvai.bence1@gmail.com>
2018-11-20 13:45:52 +0100

    Implement 'Add new answer' feature

--- server.py
+++ server.py
@@ -42,17 +42,28 @@ def route_new_answer(question_id):
     return render_template('new_answer.html', question_id=question_id)
 
 
-@app.route("/question/<question_id>")
+@app.route("/question/<question_id>", methods=['GET' , 'POST'])
 def route_question(question_id):
-    every_question = import_database("question")
-    every_answer = import_database("answer")
+    if request.method == 'GET':
+        every_question = import_database("question")
+        every_answer = import_database("answer")
 
-    current_answers = data_manager.get_answers_for_question(question_id, every_answer)
-    current_question = data_manager.get_question_by_id(question_id, every_question)
+        current_answers = data_manager.get_answers_for_question(question_id, every_answer)
+        current_question = data_manager.get_question_by_id(question_id, every_question)
 
-    return render_template('show_question.html', question_id=question_id,
+        return render_template('show_question.html', question_id=question_id,
                            question=current_question, answers=current_answers)
-
+    else:
+        new_answer = {
+            'id': "",
+            'submission_time': "",
+            'vote_number': 0,
+            'question_id': question_id,
+            'message': request.form['message'],
+            'image': request.form['image']
+        }
+        export_new_data_to_database(new_answer, "answer")
+        return redirect('/question/'+question_id)
 
 if __name__ == '__main__':
     app.static_folder = 'static'
