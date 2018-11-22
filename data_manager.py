d64302569cdbdb4fc4439493e54c2834358fe27a
Katalin Csortos <katalin.csortos@gmail.com>
2018-11-22 17:43:54 +0100

    fix: renamed variables for cleaner code

--- data_manager.py
+++ data_manager.py
@@ -60,20 +60,20 @@ def convert_counter_to_int(database, key):
         data[key] = int(data[key])
 
 
-def view_counter(question_id, count):
+def view_counter(question_id, increment):
     every_question = import_database("question")
     current_question = get_question_by_id(question_id, every_question)
     remove_data_by_id(every_question, question_id, 'id')
 
-    current_question['view_number'] += count
+    current_question['view_number'] += increment
     every_question.append(current_question)
     export_all_data("question", every_question)
 
 
-def vote_counter(id, database, updown):
+def vote_counter(data_id, database, up_or_down):
     for data in database:
-        if data['id'] == id:
-            if updown == "up":
+        if data['id'] == data_id:
+            if up_or_down == "up":
                 data['vote_number'] += 1
             else:
                 data['vote_number'] -= 1
